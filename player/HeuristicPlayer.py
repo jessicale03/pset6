501839d588fae2539f17799aa06ae73a66be1e1b
aariana12 <90168628+aariana12@users.noreply.github.com>
2023-12-14 00:52:31 -0500

    created heuristicplayer kinda

---
+++ player/HeuristicPlayer.py
@@ -0,0 +1,15 @@
+from .Player import Player
+
+class HeuristicPlayer(Player):
+    """
+    inherits from the abstract class
+    represent human player interacting from the terminal 
+    """
+
+    def __init__(self, curr_player, board):
+        super().__init__(curr_player, board)
+        # self._worker_input = "Select a worker to move (A, B, Y, Z): "
+        # # self._move_input = "Select a direction to move (n, ne, e, se, s, sw, w, nw): "
+        # self._direction_input = "Select a direction to build (n, ne, e, se, s, sw, w, nw)"
+        self._valid_workers = {'A', 'B', 'Y', 'Z'}
+        self._valid_directions = {'n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw'}
